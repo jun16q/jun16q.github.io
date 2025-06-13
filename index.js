2cf86eaa26545282a00fffe4ce313c6a4f1d55fc
Jun <cornelwu93@gmail.com>
2025-06-14 00:21:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Jun\AppData\Local\Temp\tmpftbv2nr4.js
+// include: C:\Users\Jun\AppData\Local\Temp\tmphys_dxh0.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -208,21 +208,21 @@ Module['FS_createPath']("/", "rsc", true, true);
 
   })();
 
-// end include: C:\Users\Jun\AppData\Local\Temp\tmpftbv2nr4.js
-// include: C:\Users\Jun\AppData\Local\Temp\tmp_a3emaj5.js
+// end include: C:\Users\Jun\AppData\Local\Temp\tmphys_dxh0.js
+// include: C:\Users\Jun\AppData\Local\Temp\tmpvs6e_9hx.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Jun\AppData\Local\Temp\tmp_a3emaj5.js
-// include: C:\Users\Jun\AppData\Local\Temp\tmpj9yiqsc9.js
+  // end include: C:\Users\Jun\AppData\Local\Temp\tmpvs6e_9hx.js
+// include: C:\Users\Jun\AppData\Local\Temp\tmptqc1kbg9.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Jun\AppData\Local\Temp\tmpj9yiqsc9.js
+  // end include: C:\Users\Jun\AppData\Local\Temp\tmptqc1kbg9.js
 
 
 var arguments_ = [];
