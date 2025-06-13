82071ae364ffceb86c592164159123e1c37b2027
Jun <cornelwu93@gmail.com>
2025-06-14 00:45:13 +0800

    test

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Jun\AppData\Local\Temp\tmprhupjl8s.js
+// include: C:\Users\Jun\AppData\Local\Temp\tmpam_pm1b3.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -208,21 +208,21 @@ Module['FS_createPath']("/", "rsc", true, true);
 
   })();
 
-// end include: C:\Users\Jun\AppData\Local\Temp\tmprhupjl8s.js
-// include: C:\Users\Jun\AppData\Local\Temp\tmpm4b6zewx.js
+// end include: C:\Users\Jun\AppData\Local\Temp\tmpam_pm1b3.js
+// include: C:\Users\Jun\AppData\Local\Temp\tmp_nzrow6n.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Jun\AppData\Local\Temp\tmpm4b6zewx.js
-// include: C:\Users\Jun\AppData\Local\Temp\tmpjl5ldsyk.js
+  // end include: C:\Users\Jun\AppData\Local\Temp\tmp_nzrow6n.js
+// include: C:\Users\Jun\AppData\Local\Temp\tmpegqvd4xk.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Jun\AppData\Local\Temp\tmpjl5ldsyk.js
+  // end include: C:\Users\Jun\AppData\Local\Temp\tmpegqvd4xk.js
 
 
 var arguments_ = [];
