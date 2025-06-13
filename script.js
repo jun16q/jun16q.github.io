cc9d0805e9effc933aee612a5ed6ca560b26929a
Jun <cornelwu93@gmail.com>
2025-06-14 01:06:22 +0800

    t

--- script.js
+++
@@ -1,7 +0,0 @@
-const img = document.getElementById("mainImage");
-const audio = document.getElementById("audioPlayer");
-
-img.addEventListener("click", () => {
-  audio.currentTime = 0;
-  audio.play();
-});
